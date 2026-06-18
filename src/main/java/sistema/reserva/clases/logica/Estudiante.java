9b54b109057a402bb7db765a0fe396006e214c64
b33nj4 <bensilva2024@udec.cl>
2026-06-18 03:31:04 -0400

    Agregación de atributos de clase Estudiante.

--- src/main/java/sistema/reserva/clases/logica/Estudiante.java
+++ src/main/java/sistema/reserva/clases/logica/Estudiante.java
@@ -1,4 +1,13 @@
 package sistema.reserva.clases.logica;
 
+import java.util.List;
+
 public class Estudiante {
+    private String nombre;
+    private String apellidos;
+    private String email;
+
+    private String materiaDeseada;
+    private List<String> horarioDisp;
+    private int presupuesto;
 }
