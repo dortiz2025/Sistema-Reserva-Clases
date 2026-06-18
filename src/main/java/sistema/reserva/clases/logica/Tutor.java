fec0f2ce93c44cc4463f0f005550328ad7bfb2df
b33nj4 <bensilva2024@udec.cl>
2026-06-18 02:49:55 -0400

    Agregación constructor de Tutor y setters de los atributos que podrían llegar a variar

--- src/main/java/sistema/reserva/clases/logica/Tutor.java
+++ src/main/java/sistema/reserva/clases/logica/Tutor.java
@@ -6,11 +6,28 @@ public class Tutor {
     private String apellidos;
     private String email;
     private String materia;
+    private String horariosDisp;
     private int tarifa;
     private int maxAlum;
-    private String horariosDisp;
 
-    public Tutor(){
+    public Tutor(String nombre, String apellidos, String email, String materia){
 
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.materia = materia;
+    }
+
+
+    public void setHorariosDisp(String horariosDisp){
+        this.horariosDisp = horariosDisp;
+    }
+
+    public void setTarifa(int tarifa){
+        this.tarifa = tarifa;
+    }
+
+    public void setMaxAlum(int maxAlum){
+        this.maxAlum = maxAlum;
     }
 }
