0435de16c0568afe524065afdaf11b720419967f
b33nj4 <bensilva2024@udec.cl>
2026-06-18 02:38:15 -0400

    Agregación de atributos de clase Tutor

--- src/main/java/sistema/reserva/clases/logica/Tutor.java
+++ src/main/java/sistema/reserva/clases/logica/Tutor.java
@@ -1,4 +1,16 @@
 package sistema.reserva.clases.logica;
+ import java.util.List;
 
 public class Tutor {
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String materia;
+    private int tarifa;
+    private int maxAlum;
+    private String horariosDisp;
+
+    public Tutor(){
+
+    }
 }
