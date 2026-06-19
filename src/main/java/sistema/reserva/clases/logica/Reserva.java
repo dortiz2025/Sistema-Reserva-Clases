bf87a21301e748189628f825bb9305d116addf98
b33nj4 <bensilva2024@udec.cl>
2026-06-18 23:09:36 -0400

    Crear clase Reserva con atributos estudiante, tutor, materia y horario.

---
+++ src/main/java/sistema/reserva/clases/logica/Reserva.java
@@ -0,0 +1,8 @@
+package sistema.reserva.clases.logica;
+
+public class Reserva {
+    private Estudiante estudiante;
+    private Tutor tutor;
+    private String materia;
+    private String horario;
+}
