cdc5e52794b259548dd73f2079d9227307462ee7
dortiz2025 <dortiz2025@inf.udec.cl>
2026-07-01 11:28:03 -0400

    Se añade documentación completa a clase abstracta Perfil.

--- src/main/java/sistema/reserva/clases/logica/Perfil.java
+++ src/main/java/sistema/reserva/clases/logica/Perfil.java
@@ -10,24 +10,48 @@ public abstract class Perfil {
     private String nombre;
     private String email;
 
+    /**
+     * Constructor de la clase.
+     * @param nombre Nombre del perfil.
+     * @param email Email del perfil.
+     * @throws CorreoInvalidoException Se lanza excepción si el correo es inválido,
+     * es decir, si no contiene un @.
+     */
     public Perfil(String nombre, String email) throws CorreoInvalidoException {
         this.nombre = nombre;
         if (email.contains("@")) this.email = email;
         else throw new CorreoInvalidoException("Email invalido");
     }
 
+    /**
+     * Getter de nombre.
+     * @return Nombre del perfil.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Setter de nombre.
+     * @param nombre Nuevo nombre del perfil.
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Getter del email.
+     * @return Nombre del perfil.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter del perfil.
+     * @param email Nuevo email asociado.
+     * @throws CorreoInvalidoException Verificación de @ en el email. Si no lo contiene, se lanza excepción.
+     */
     public void setEmail(String email) throws CorreoInvalidoException {
         if (email.contains("@")) this.email = email;
         else throw new CorreoInvalidoException("Email invalido");
