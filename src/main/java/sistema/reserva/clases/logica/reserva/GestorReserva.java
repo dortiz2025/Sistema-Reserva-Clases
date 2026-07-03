5c91d1db03b60f4b13d5ecc5d158eda04ade8016
b33nj4 <bensilva2024@udec.cl>
2026-07-03 00:18:18 -0400

    Imports necesarios y creación de clase GestorReserva.

---
+++ src/main/java/sistema/reserva/clases/logica/reserva/GestorReserva.java
@@ -0,0 +1,14 @@
+package sistema.reserva.clases.logica.reserva;
+
+import sistema.reserva.clases.excepciones.CupoExcedidoException;
+import sistema.reserva.clases.excepciones.EstudianteYaInscritoException;
+import sistema.reserva.clases.logica.BloqueHorario;
+import sistema.reserva.clases.logica.Estudiante;
+import sistema.reserva.clases.logica.Tutor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorReserva{
+
+}
