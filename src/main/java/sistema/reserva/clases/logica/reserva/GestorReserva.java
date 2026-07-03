9d3ea3368597726a0e39a6684c44f407cb629ebb
b33nj4 <bensilva2024@udec.cl>
2026-07-03 00:48:32 -0400

    Se agrega método en clase GestorReserva que registra una reserva utilizando las validaciones necesarias y lanzando excepciones,

--- src/main/java/sistema/reserva/clases/logica/reserva/GestorReserva.java
+++ src/main/java/sistema/reserva/clases/logica/reserva/GestorReserva.java
@@ -9,12 +9,46 @@ import sistema.reserva.clases.logica.Tutor;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestorReserva{
 public class GestorReserva {
     private List<Reserva> listaReservas;
 
     public GestorReserva() {
         this.listaReservas = new ArrayList<>();
+    }
+    /**
+     * Intenta registrar una nueva reserva validando las reglas de negocio.
+     */
+    public void registrarReserva(Estudiante estudiante, Tutor tutor, String materia, BloqueHorario horario)
+            throws EstudianteYaInscritoException, CupoExcedidoException {
+
+        for (Reserva r : listaReservas){
+            boolean esMismoEstudiante = r.getEstudiante().getMatricula().equals(estudiante.getMatricula());
+            boolean esMismoHorario = r.getHorario().equals(horario);
+            boolean noEstaCancelada = !r.getNombreEstado().equals("Cancelada"); // Asumiendo que getNombreEstado() devuelve "Cancelada"
+
+            if (esMismoEstudiante && esMismoHorario && noEstaCancelada) {
+                throw new EstudianteYaInscritoException("El estudiante " + estudiante.getNombre() +
+                        " ya tiene una reserva en el " + horario.toString());
+            }
+        }
+
+        int alumnosInscritosEnBloque = 0;
+
+        for (Reserva r : listaReservas) {
+            boolean esMismoTutor = r.getTutor().getId().equals(tutor.getId());
+            boolean esMismoHorario = r.getHorario().equals(horario);
+            boolean noEstaCancelada = !r.getNombreEstado().equals("Cancelada");
+
+            if (esMismoTutor && esMismoHorario && noEstaCancelada) {
+                alumnosInscritosEnBloque++;
+            }
+        }
+        if (alumnosInscritosEnBloque >= tutor.getMaxAlum()) {
+            throw new CupoExcedidoException("El tutor " + tutor.getNombre() +
+                    " ya alcanzó su límite máximo de " + tutor.getMaxAlum() + " alumnos en ese horario.");
+        }
+        Reserva nuevaReserva = new Reserva(estudiante, tutor, materia, horario);
+        listaReservas.add(nuevaReserva);
     }
 
 }
