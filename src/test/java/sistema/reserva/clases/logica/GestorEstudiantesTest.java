3a36be3725ceaa3da4e02a30b9422794e076c5fa
b33nj4 <bensilva2024@udec.cl>
2026-07-06 18:43:50 -0400

    Agregamos testRegistrarEstudianteExitoso y testRegistrarEstudianteDuplicadoLanzaExcepcion en GestorEstudiantesTest (este sí, el anterior se me fueron solos los imports en el commit).

--- src/test/java/sistema/reserva/clases/logica/GestorEstudiantesTest.java
+++ src/test/java/sistema/reserva/clases/logica/GestorEstudiantesTest.java
@@ -12,4 +12,49 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class GestorEstudiantesTest {
 
+    private GestorEstudiantes gestor;
+
+    /**
+     * Inicializa el estado antes de cada prueba para asegurar independencia de contexto.
+     */
+    @BeforeEach
+    void setUp() {
+        gestor = new GestorEstudiantes();
+    }
+
+    /**
+     * Valida que un estudiante válido se registre correctamente en el sistema.
+     * La verificación se realiza consultando la colección interna del gestor.
+     */
+    @Test
+    void testRegistrarEstudianteExitoso() throws CorreoInvalidoException, CorreoYaRegistradoException {
+        // Arrange
+        Estudiante estudiante = new Estudiante("Catalina Silva", "cata.silva@udec.cl");
+
+        // Act
+        gestor.registrarEstudiante(estudiante);
+
+        // Assert
+        // Se utiliza el método heredado de GestorPerfil
+        assertTrue(gestor.obtenerPerfiles().contains(estudiante), "El estudiante debe encontrarse en la colección del gestor tras su registro.");
+    }
+
+    /**
+     * Comprueba que el sistema rechace el registro de un estudiante nuevo
+     * que utilice un correo ya existente, lanzando la excepción correspondiente.
+     */
+    @Test
+    void testRegistrarEstudianteDuplicadoLanzaExcepcion() throws CorreoInvalidoException, CorreoYaRegistradoException {
+        //Se instancian dos estudiantes distintos pero con el mismo correo electrónico
+        Estudiante estudiante1 = new Estudiante("Carlos Vera", "carlos.v@udec.cl");
+        Estudiante estudiante2 = new Estudiante("Carlos Varela", "carlos.v@udec.cl");
+
+        //El registro del primer estudiante es exitoso
+        gestor.registrarEstudiante(estudiante1);
+
+        //El registro del segundo estudiante falla en la validación de correo
+        assertThrows(CorreoYaRegistradoException.class, () -> {
+            gestor.registrarEstudiante(estudiante2);
+        }, "Debe lanzar CorreoYaRegistradoException al ingresar un nuevo estudiante con un correo ya en uso.");
+    }
 }
