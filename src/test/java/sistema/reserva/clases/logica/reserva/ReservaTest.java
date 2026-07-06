8dfaed7a1310b86e7c801b265afe72e879a06ef7
b33nj4 <bensilva2024@udec.cl>
2026-07-06 18:47:57 -0400

    Agregamos imports, javadoc y testNuevaReservaInicialComoPendiente en clase ReservaTest.

--- src/test/java/sistema/reserva/clases/logica/reserva/ReservaTest.java
+++ src/test/java/sistema/reserva/clases/logica/reserva/ReservaTest.java
@@ -1,7 +1,38 @@
 package sistema.reserva.clases.logica.reserva;
 
+import org.junit.jupiter.api.Test;
+import sistema.reserva.clases.excepciones.CorreoInvalidoException;
+import sistema.reserva.clases.logica.Estudiante;
+import sistema.reserva.clases.logica.Tutor;
+import sistema.reserva.clases.logica.bloquehorario.Bloque;
+import sistema.reserva.clases.logica.bloquehorario.BloqueHorario;
+import sistema.reserva.clases.logica.bloquehorario.DiaSemana;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Pruebas unitarias para la entidad Reserva y su ciclo de vida.
+ */
 class ReservaTest {
 
+    /**
+     * Verifica que toda nueva reserva se inicialice por defecto en EstadoPendiente.
+     */
+    @Test
+    void testNuevaReservaIniciaComoPendiente() throws CorreoInvalidoException {
+        // Arrange
+        Estudiante estudiante = new Estudiante("Alumno DOO", "alumno@udec.cl");
+        Tutor tutor = new Tutor("Tutor Estrella", "tutor@udec.cl", 15000, 20);
+
+        BloqueHorario bloqueHorario = new BloqueHorario(DiaSemana.LUNES, Bloque.BLOQUE_3);
+
+        // Act: Se utiliza LocalDate para la fecha.
+        // Si Bloque.BLOQUE_3 sigue en rojo, revisa si el constructor pide un BloqueHorario.
+        Reserva reserva = new Reserva(estudiante, tutor, "Matemáticas", bloqueHorario, LocalDate.parse("2026-05-04"));
+
+        // Assert
+        assertEquals(NombreEstado.PENDIENTE, reserva.getEstado(), "El estado inicial de la reserva debe ser PENDIENTE");
+    }
 }
