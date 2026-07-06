f84a3ba29cd561a08682bb2e228a770871d8cbab
b33nj4 <bensilva2024@udec.cl>
2026-07-06 18:11:39 -0400

    Agregamos testRegistrarEstudianteExitoso y testRegistrarEstudianteDuplicadoLanzaExcepcion en GestorEstudiantesTest.

--- src/test/java/sistema/reserva/clases/logica/GestorEstudiantesTest.java
+++ src/test/java/sistema/reserva/clases/logica/GestorEstudiantesTest.java
@@ -1,7 +1,15 @@
 package sistema.reserva.clases.logica;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sistema.reserva.clases.excepciones.CorreoInvalidoException;
+import sistema.reserva.clases.excepciones.CorreoYaRegistradoException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Pruebas unitarias para el gestor encargado del ciclo de vida de los Estudiantes.
+ */
 class GestorEstudiantesTest {
 
 }
