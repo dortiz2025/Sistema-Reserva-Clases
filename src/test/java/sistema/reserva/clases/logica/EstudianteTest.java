bc7156d5003ef5d7b0f1c6f2edc0ef9218b183da
b33nj4 <bensilva2024@udec.cl>
2026-07-06 12:58:27 -0400

    Se agrega testCreacionEstudianteExitosa, que verifica que se asginen bien los datos como nombre y email.

--- src/test/java/sistema/reserva/clases/logica/EstudianteTest.java
+++ src/test/java/sistema/reserva/clases/logica/EstudianteTest.java
@@ -6,5 +6,15 @@ import sistema.reserva.clases.excepciones.CorreoInvalidoException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EstudianteTest {
+    @Test
+    void testCreacionEstudianteExitosa() throws CorreoInvalidoException {
+        String nombreEsperado = "Luis Soto";
+        String emailEsperado = "luis.soto@email.com";
 
+        Estudiante estudiante = new Estudiante(nombreEsperado, emailEsperado);
+
+        //Se verifica que los datos heredados de Perfil se asignen correctamente
+        assertEquals(nombreEsperado, estudiante.getNombre(), "El nombre del estudiante no coincide.");
+        assertEquals(emailEsperado, estudiante.getEmail(), "El email del estudiante no coincide.");
+    }
 }
