278877ad7b6a25eb969b63a49afe08fe8214b35c
b33nj4 <bensilva2024@udec.cl>
2026-07-06 02:22:06 -0400

    Imports necesarios en EstudianteTest.

--- src/test/java/sistema/reserva/clases/logica/EstudianteTest.java
+++ src/test/java/sistema/reserva/clases/logica/EstudianteTest.java
@@ -1,5 +1,8 @@
 package sistema.reserva.clases.logica;
 
+import org.junit.jupiter.api.Test;
+import sistema.reserva.clases.excepciones.CorreoInvalidoException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EstudianteTest {
