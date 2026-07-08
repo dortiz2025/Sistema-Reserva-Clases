6489a4cbef49f6f1b46ec8fb43705befae680bff
b33nj4 <bensilva2024@udec.cl>
2026-07-08 18:11:43 -0400

    Se realizan imports correspondientes, se inicializa contexto y se agrega javadoc en clase EstadoCompletadaTest.

--- src/test/java/sistema/reserva/clases/logica/reserva/EstadoCompletadaTest.java
+++ src/test/java/sistema/reserva/clases/logica/reserva/EstadoCompletadaTest.java
@@ -1,7 +1,40 @@
 package sistema.reserva.clases.logica.reserva;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sistema.reserva.clases.excepciones.CorreoInvalidoException;
+import sistema.reserva.clases.logica.Estudiante;
+import sistema.reserva.clases.logica.Tutor;
+import sistema.reserva.clases.logica.bloquehorario.Bloque;
+import sistema.reserva.clases.logica.bloquehorario.BloqueHorario;
+import sistema.reserva.clases.logica.bloquehorario.DiaSemana;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class EstadoCompletadaTest {
+/**
+ * Pruebas unitarias para el comportamiento del estado Completada dentro del ciclo de vida de una Reserva.
+ */
+public class EstadoCompletadaTest {
+
+    private EstadoCompletada estadoCompletada;
+    private Reserva reserva;
+
+    /**
+     * Inicializa el estado y los objetos de negocio necesarios antes de cada prueba para asegurar independencia de contexto.
+     * @throws CorreoInvalidoException Si el formato del correo de las entidades Estudiante o Tutor no es válido.
+     */
+    @BeforeEach
+    public void setUp() throws CorreoInvalidoException {
+
+        estadoCompletada = new EstadoCompletada();
+        Estudiante estudiante = new Estudiante("Alumno DOO", "alumno@udec.cl");
+        Tutor tutor = new Tutor("Tutor", "tutor@udec.cl", 15000, 20);
+
+        BloqueHorario bloqueHorario = new BloqueHorario(DiaSemana.LUNES, Bloque.BLOQUE_3);
+
+        this.reserva = new Reserva(estudiante, tutor, "Matemáticas", bloqueHorario, LocalDate.parse("2026-05-04"));
+    }
 
 }
